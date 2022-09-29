ad7d3963e00090aea612cf93dd06231268137a9c
Hugo Santos Suárez <hugsansua2003@gmail.com>
2022-09-29 10:28:57 +0200

    29-09-2022

---
+++ PMDM/Proxectos/app/src/main/java/com/example/ciclodevida/MainActivity.java
@@ -0,0 +1,119 @@
+package com.example.ciclodevida;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.ciclodevida.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Código Ejercicio LOGS
+
+        Log.i("ON-CREATE", "Estoy en On-Create!!");
+
+        // ------------------ //
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.i("ON-START", "Estoy ON-Start");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.i("ON-PAUSE", "Estoy ON-Pause");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.i("ON-STOP", "Estoy ON-Stop");
+    }
+
+    @Override
+    public void onRestart() {
+        super.onRestart();
+        Log.i("ON-RESTART", "Estoy ON-Restart");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.i("ON-Resume", "Estoy ON-Resume");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i("ON-DESTROY", "Estoy ON-Destroy");
+    }
+}
