fb6fc890b14fb3143ec26947edcaec435de3ad6b
Hugo Santos Suárez <hugsansua2003@gmail.com>
2022-09-30 09:40:06 +0200

    30-09-2022

---
+++ PMDM/Proxectos/Intents/app/src/main/java/com/example/intents/Activity2.java
@@ -0,0 +1,13 @@
+package com.example.intents;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Activity2 extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity2_main);
+    }
+}
---
+++ PMDM/Proxectos/Intents/app/src/main/java/com/example/intents/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.intents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Intent ejemplo = new Intent(this, Activity2.class);
+        startActivity(ejemplo);
+    }
+}
