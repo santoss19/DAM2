d7936556f5590cfa471d41e7e4821e6d279cdf34
Hugo Santos Suárez <hugsansua2003@gmail.com>
2022-10-03 13:57:13 +0200

    03-10-2022

--- DI/Proxectos/ProyectoVentana/src/controlador/AccionBoton2.java
+++ DI/Proxectos/ProyectoVentana/src/controlador/AccionBoton2.java
@@ -6,10 +6,12 @@ import java.util.List;
 import java.util.ArrayList;
 
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class AccionBoton2 implements ActionListener {
 
     JPanel jp = null;
+    List<String> textos = new ArrayList<>();
 
     public AccionBoton2(JPanel jPanel) {
         this.jp = jPanel;
@@ -18,17 +20,36 @@ public class AccionBoton2 implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         
         Component[] componentes = jp.getComponents();
-        List<Component> componentesText = new ArrayList<>();
+        List<JTextField> componentesText = new ArrayList<>();
+        int cuenta = 0;
+        int ronda = 1;
 
-        for(int i = 0; componentes[i].getClass().getName() == "java.swing.JTextField"; i++) {
-            componentesText.add(componentes[i]);
+        for(int i = 0; i < componentes.length ; i++) {
+            if(componentes[i].getClass().getName().equals("javax.swing.JTextField")) {
+                componentesText.add((JTextField)componentes[i]);
+            }
+        }
+ 
+        for(JTextField texto : componentesText) {
+            for(JTextField texto2 : componentesText) {
+                cuenta++;
+                texto.getText().equals(texto2.getText());
+            }
+            if(cuenta > 2 && ronda == 1) {
+                textos.add("A tiene " + cuenta + " iguales a el.");
+            } else if(cuenta > 2 && ronda == 2) {
+                textos.add("B tiene " + cuenta + " iguales a el.");
+            } else if(cuenta > 2 && ronda == 3) {
+                textos.add("C tiene " + cuenta + " iguales a el.");
+            } else if(cuenta > 2 && ronda == 4) {
+                textos.add("D tiene " + cuenta + " iguales a el.");
+            }
+            ronda ++;
+            cuenta = 0;
         }
 
-        for(int i = 0; i < componentesText.size(); i++) {
-            JPanel next = componentesText.iterator().next();
-            for(int j = 0; j < componentesText.size(); i++) {
-                next.get
-            }
+        for(String text : textos) {
+            System.out.println(text);
         }
     }
 }
