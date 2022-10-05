e22e0c2f487fd8d5b890ade9bf70af60da8075b6
Hugo Santos Suárez <hugsansua2003@gmail.com>
2022-10-05 09:19:17 +0200

    05-10-2022

--- DI/Proxectos/ProyectoVentana/src/modelo/PVentana.java
+++ DI/Proxectos/ProyectoVentana/src/modelo/PVentana.java
@@ -1,25 +1,7 @@
 package modelo;
-import controlador.AccionBoton1;
-import controlador.AccionBoton2;
-import controlador.AccionBoton2_BL;
 
 public class PVentana {
-
-    public PVentana() {}
-
-    public void FLVentana() {
-        FLVentana fl = new FLVentana();
-        
-    }
-
-    public void BLVentana() {
-        BLVentana bl = new BLVentana();
-        bl.bot1.addActionListener(new AccionBoton1(bl));
-        bl.bot2.addActionListener(new AccionBoton2_BL(bl.panelN, bl.panelS, bl.panelC));
-    }
-
     public static void main(String[] args) {
-        PVentana pv = new PVentana();
-        pv.FLVentana();
+        new BLVentana();
     }
 }
